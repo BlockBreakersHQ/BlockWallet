fb57fa0312f13527516e84bfce05526553f02cb6
Andy <andy@Andys-MacBook-Pro.local>
2023-01-23 01:09:22 -0700

    Added buttons to wallets tab

--- src/Views/wallets.rs
+++ src/Views/wallets.rs
@@ -1,6 +1,8 @@
+use adw::prelude::*;
 use gtk::prelude::*;
 use gtk::{Button, Orientation, Image};
 use std::sync::{Arc, Mutex};
+
 
 use crate::configuration::application_settings::*;
 use crate::currencies::eth::EthereumWallet;
@@ -9,8 +11,6 @@ use crate::currencies::btc::BitcoinWallet;
 pub fn wallet_view(app_settings: ApplicationSettings) -> gtk::Box {
     let btc_data_displayed = Arc::new(Mutex::new(false));
     let eth_data_displayed = Arc::new(Mutex::new(false));
-
-    let app_settings_clone = app_settings.clone();
 
     let btc_wallets = app_settings.btc_wallets;
     let eth_wallets = app_settings.eth_wallets;
@@ -45,7 +45,7 @@ pub fn wallet_view(app_settings: ApplicationSettings) -> gtk::Box {
     asset_box.lock().unwrap().append(&eth_button);
 
     let btc_currency_details = populate_btc_currency_details(&btc_wallets);
-    let eth_currency_details = populate_eth_currency_details(&eth_wallets, app_settings_clone);
+    let eth_currency_details = populate_eth_currency_details(&eth_wallets);
     asset_box.lock().unwrap().insert_child_after(&btc_currency_details, Some(&btc_button));
     asset_box.lock().unwrap().insert_child_after(&eth_currency_details, Some(&eth_button));
     let btc_currency_details_clone = btc_currency_details.clone();
@@ -79,23 +79,28 @@ pub fn wallet_view(app_settings: ApplicationSettings) -> gtk::Box {
 }
 
 fn populate_btc_currency_details(btc_wallets: &Vec<BitcoinWallet>) -> gtk::Box {
-    let widgets = gtk::Box::builder()    
+    let widgets = gtk::Box::builder()
         .orientation(Orientation::Vertical)
         .visible(false)
         .build();
     
     for btcw in btc_wallets {
+        let wallet_box = gtk::Box::builder()
+            .orientation(Orientation::Vertical)
+            .visible(false)
+            .build();
+            
         let btc_mnemonic = match &btcw.mnemonic {
             Some(mnemonic) => format!("Mnemonic: {}", mnemonic),
             None => String::from("Uninitialized")
         };
-
         let btc_mnemonic_label = gtk::Label::builder()
             .label(&btc_mnemonic)
             .halign(gtk::Align::Start)
             .max_width_chars(50)
             .margin_start(12)
             .margin_end(12)
+            .margin_top(12)
             .wrap(true)
             .wrap_mode(pango::WrapMode::Char)
             .build();
@@ -104,6 +109,7 @@ fn populate_btc_currency_details(btc_wallets: &Vec<BitcoinWallet>) -> gtk::Box {
             Some(address) => format!("Address: {}", address),
             None => String::from("Uninitialized")
         };
+        
         let btc_address_label = gtk::Label::builder()
             .label(&btc_address)
             .halign(gtk::Align::Start)
@@ -143,27 +149,82 @@ fn populate_btc_currency_details(btc_wallets: &Vec<BitcoinWallet>) -> gtk::Box {
             .wrap_mode(pango::WrapMode::Char)
             .build();
 
-        let qr_code = btcw.generate_qr_address().unwrap();
-        let qr_image = Image::from_paintable(Some(&qr_code));
-        qr_image.set_pixel_size(300);
+        let qr_button = Button::builder()
+            .label("Show QR Code")
+            .margin_top(6)
+            .margin_bottom(12)
+            .margin_start(12)
+            .margin_end(12)
+            .build();
+
+        let wallet_name = match &btcw.wallet_name {
+            Some(wn) => wn,
+            None     => "Unnamed Wallet"
+        };
+
+        let wallet_button = Button::builder()
+            .label(&wallet_name)
+            .margin_top(6)
+            .margin_bottom(12)
+            .margin_start(12)
+            .margin_end(12)
+            .build();
         
-        widgets.append(&btc_mnemonic_label);
-        widgets.append(&btc_address_label);
-        widgets.append(&btc_private_key_label);
-        widgets.append(&btc_public_key_label);
-        widgets.append(&qr_image);
+        let qr_box = btc_qr_box(btcw);
+        let qr_box_1 = qr_box.clone();
+
+        qr_button.connect_clicked(move |button| {
+            if qr_box.get_visible() == false {
+                qr_box.set_visible(true);
+                button.set_label("Hide QR Code");
+            }
+            else {
+                qr_box.set_visible(false);
+                button.set_label("Show QR Code");
+            }
+        });
+
+        let expander = adw::ExpanderRow::builder()
+            .title(&wallet_name)
+            .margin_start(12)
+            .margin_end(12)
+            .height_request(40)
+            .build();
+
+        expander.add_row(&btc_mnemonic_label);
+        expander.add_row(&btc_address_label);
+        expander.add_row(&btc_private_key_label);
+        expander.add_row(&btc_public_key_label);
+        wallet_box.append(&expander);
+        wallet_box.append(&qr_box_1);
+        wallet_box.append(&qr_button);
+        let wallet_box_1 = wallet_box.clone();
+
+        wallet_button.connect_clicked(move |button| {
+            if wallet_box.get_visible() == false {
+                wallet_box.set_visible(true);
+                button.set_label("Hide wallet details");
+            }
+            else {
+                wallet_box.set_visible(false);
+                button.set_label("Show wallet details");
+            }
+        });
+        
+        widgets.append(&wallet_box_1);
+        widgets.append(&wallet_button);
     }
     return widgets;
 }
 
-fn populate_eth_currency_details(eth_wallets: &Vec<EthereumWallet>, app_settings: ApplicationSettings) -> gtk::Box {
+fn populate_eth_currency_details(eth_wallets: &Vec<EthereumWallet>/*, app_settings: ApplicationSettings*/) -> gtk::Box {
     let widgets = gtk::Box::builder()
         .orientation(Orientation::Vertical)
         .visible(false)
         .build();
     
     for ethw in eth_wallets {
-        let wallet_details = gtk::Box::builder()
+        let wallet_box = gtk::Box::builder()
             .orientation(Orientation::Vertical)
             .visible(false)
             .build();
@@ -178,6 +239,7 @@ fn populate_eth_currency_details(eth_wallets: &Vec<EthereumWallet>, app_settings
             .max_width_chars(50)
             .margin_start(12)
             .margin_end(12)
+            .margin_top(12)
             .wrap(true)
             .wrap_mode(pango::WrapMode::Char)
             .build();
@@ -226,15 +288,98 @@ fn populate_eth_currency_details(eth_wallets: &Vec<EthereumWallet>, app_settings
             .wrap_mode(pango::WrapMode::Char)
             .build();
 
-        let qr_code = ethw.generate_qr_address().unwrap();
-        let qr_image = Image::from_paintable(Some(&qr_code));
-        qr_image.set_pixel_size(300);
+        let qr_button = Button::builder()
+            .label("Show QR Code")
+            .margin_top(6)
+            .margin_bottom(12)
+            .margin_start(12)
+            .margin_end(12)
+            .build();
+
+        let wallet_name = match &ethw.wallet_name {
+            Some(wn) => wn,
+            None     => "Unnamed Wallet"
+        };
+
+        let wallet_button = Button::builder()
+            .label(&wallet_name)
+            .margin_top(6)
+            .margin_bottom(12)
+            .margin_start(12)
+            .margin_end(12)
+            .build();
         
-        widgets.append(&eth_mnemonic_label);
-        widgets.append(&eth_address_label);
-        widgets.append(&eth_private_key_label);
-        widgets.append(&eth_public_key_label);
-        widgets.append(&qr_image);
+        let qr_box = eth_qr_box(ethw);
+        let qr_box_1 = qr_box.clone();
+
+        qr_button.connect_clicked(move |button| {
+            if qr_box.get_visible() == false {
+                qr_box.set_visible(true);
+                button.set_label("Hide QR Code");
+            }
+            else {
+                qr_box.set_visible(false);
+                button.set_label("Show QR Code");
+            }
+        });
+
+        let expander = adw::ExpanderRow::builder()
+            .title(&wallet_name)
+            .margin_start(12)
+            .margin_end(12)
+            .height_request(40)
+            .build();
+
+        expander.add_row(&eth_mnemonic_label);
+        expander.add_row(&eth_address_label);
+        expander.add_row(&eth_private_key_label);
+        expander.add_row(&eth_public_key_label);
+        wallet_box.append(&expander);
+        wallet_box.append(&qr_box_1);
+        wallet_box.append(&qr_button);
+        let wallet_box_1 = wallet_box.clone();
+
+        wallet_button.connect_clicked(move |button| {
+            if wallet_box.get_visible() == false {
+                wallet_box.set_visible(true);
+                button.set_label("Hide wallet details");
+            }
+            else {
+                wallet_box.set_visible(false);
+                button.set_label("Show wallet details");
+            }
+        });
+        
+        widgets.append(&wallet_box_1);
+        widgets.append(&wallet_button);
     }
     return widgets;
+}
+
+fn btc_qr_box(btcw: &BitcoinWallet) -> gtk::Box {
+    let qr_box = gtk::Box::builder()
+            .orientation(Orientation::Vertical)
+            .visible(false)
+            .build();
+
+    let qr_code = btcw.generate_qr_address().unwrap();
+    let qr_image = Image::from_paintable(Some(&qr_code));
+    qr_image.set_pixel_size(300);
+
+    qr_box.append(&qr_image);
+    return qr_box;
+}
+
+fn eth_qr_box(ethw: &EthereumWallet) -> gtk::Box {
+    let qr_box = gtk::Box::builder()
+            .orientation(Orientation::Vertical)
+            .visible(false)
+            .build();
+
+    let qr_code = ethw.generate_qr_address().unwrap();
+    let qr_image = Image::from_paintable(Some(&qr_code));
+    qr_image.set_pixel_size(300);
+
+    qr_box.append(&qr_image);
+    return qr_box;
 }
